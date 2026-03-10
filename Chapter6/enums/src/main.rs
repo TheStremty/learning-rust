3d1b2afc559dc27610bb8ed902589a5f4229ee8a
TheStremty <mrmax2811@gmail.com>
2026-03-10 17:13:26 +0100

    Chapter 6: Enums, Match and Option complete

--- Chapter6/enums/src/main.rs
+++ Chapter6/enums/src/main.rs
@@ -2,31 +2,46 @@ enum IpAddr {
     V4(u8,u8,u8,u8), // Opcja nr 1: cztery małe liczby
     V6(String),      // Opcja nr 2: jeden długi napis
 }
-
+impl IpAddr {
+    fn print_ip(&self) {
+        match self {
+            IpAddr::V4(a,b,c,d) => {println!("IPV4: {}.{}.{}.{}", a,b,c,d)},
+            IpAddr::V6(ip) => println!("IPV6: {}", ip),
+        }
+    }
+}
 
 
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
 
-    print_ip(&home);
+    home.print_ip();
 
     if let IpAddr::V6(ip) = &loopback {
         println!("V6 pasuje {}", ip);
     }
 
-    print_ip(&loopback);
+    loopback.print_ip();
 
     if let IpAddr::V4(a, b, c, d) = &home {
         println!("V4 pasuje {}.{}.{}.{}", a,b,c,d);
     }
 
-}
+    let number:Option<u32> = Some(512);
 
-
-fn print_ip(addr: &IpAddr) {
-    match addr {
-        IpAddr::V4(a,b,c,d) => {println!("IPV4: {}.{}.{}.{}", a,b,c,d)},
-        IpAddr::V6(ip) => println!("IPV6: {}", ip),
+    if let Some(n) = number {
+        println!("liczba to: {}", n);
     }
+
+    let number:Option<u32> = None;
+
+    if let Some(n) = number {
+        println!("liczba to: {}", n);
+    }
+    else {
+        println!("Nie ma liczby!");
+    }
+
+
 }
